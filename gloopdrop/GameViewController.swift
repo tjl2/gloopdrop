6b054d94d137d3579d6bdbaff98d765f6099d04d
Tim Littlemore <tim@tjl2.com>
2021-05-12 23:20:31 +0100

    Finished Ch. 1

--- gloopdrop/GameViewController.swift
+++ gloopdrop/GameViewController.swift
@@ -13,6 +13,26 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Create teh view
+        if let view = self.view as! SKView? {
+            // Create the scene
+            //let scene = GameScene(size: view.bounds.size)
+            let scene = GameScene(size: CGSize(width: 1336, height: 1024))
+            // Set the scale mode to scale to fill the view window
+            scene.scaleMode = .aspectFill
+            // Set the background color
+            scene.backgroundColor = UIColor(red: 105/255,
+                                            green: 157/255,
+                                            blue: 181/255,
+                                            alpha: 1.0)
+            // Present the scene
+            view.presentScene(scene)
+            // Set the view options
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
 
     override var shouldAutorotate: Bool {
