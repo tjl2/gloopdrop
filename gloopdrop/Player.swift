7b1d46689f2e0239e5af0074dc27d05f4d6b61d2
Tim Littlemore <tim@tjl2.com>
2021-05-18 20:44:59 +0100

    Movement

--- gloopdrop/Player.swift
+++ gloopdrop/Player.swift
@@ -48,4 +48,9 @@ class Player: SKSpriteNode {
         // Run animation (forever)
         startAnimation(textures: walkTextures, speed: 0.25, name: PlayerAnimationType.walk.rawValue, count: 0, resize: true, restore: true)
     }
+    
+    func moveToPosition(pos: CGPoint, speed: TimeInterval) {
+        let moveAction = SKAction.move(to: pos, duration: speed)
+        run(moveAction)
+    }
 }
