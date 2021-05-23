0faa90b06f6cf16d3c74f5976084e8a09c557248
Tim Littlemore <tim@tjl2.com>
2021-05-23 09:46:58 +0100

    Gloop… drops

--- gloopdrop/Collectible.swift
+++ gloopdrop/Collectible.swift
@@ -42,4 +42,21 @@ class Collectible: SKSpriteNode {
     required init?(coder aDecoder : NSCoder) {
         fatalError("init(coderr:) has not been implemented")
     }
+    
+    // MARK: - FUNCTIONS
+    func drop(dropSpeed: TimeInterval, floorLevel: CGFloat) {
+        let pos = CGPoint(x: position.x, y: floorLevel)
+        
+        let scaleX = SKAction.scaleX(to: 1.0, duration: 1.0)
+        let scaleY = SKAction.scaleY(to: 1.3, duration: 1.0)
+        let scale = SKAction.group([scaleX, scaleY])
+        
+        let appear = SKAction.fadeAlpha(to: 1.0, duration: 0.25)
+        let moveAction = SKAction.move(to: pos, duration: dropSpeed)
+        let actionSequence = SKAction.sequence([appear, scale, moveAction])
+        
+        // Shrink first, then fall action
+        self.scale(to: CGSize(width: 0.25, height: 1.0))
+        self.run(actionSequence, withKey: "drop")
+    }
 }
--- gloopdrop/GameScene.swift
+++ gloopdrop/GameScene.swift
@@ -44,6 +44,7 @@ class GameScene: SKScene {
         let collectible = Collectible(collectibleType: CollectibleType.gloop)
         collectible.position = CGPoint(x: player.position.x, y: player.position.y * 2.5)
         addChild(collectible)
+        collectible.drop(dropSpeed: TimeInterval(1.0), floorLevel: player.frame.minY)
     }
     
     // MARK: - TOUCH HANDLING
