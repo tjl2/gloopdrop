d3ff9c29b65209623e05fd5945344f0e7fcba51a
Tim Littlemore <tim@tjl2.com>
2021-06-22 19:56:28 +0100

    Remove debug stuff

--- gloopdrop/GameViewController.swift
+++ gloopdrop/GameViewController.swift
@@ -29,9 +29,9 @@ class GameViewController: UIViewController {
             view.presentScene(scene)
             // Set the view options
             view.ignoresSiblingOrder = false
-            view.showsPhysics = true
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsPhysics = false
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
